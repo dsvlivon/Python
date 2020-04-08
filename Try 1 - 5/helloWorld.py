9e2a3dc52f60d58003c32c5215468287cbab5199
Daniel <dsvlivon@gmail.com>
2020-04-08 20:10:47 -0300

    commit inicial

---
+++ Try 1 - 5/helloWorld.py
@@ -0,0 +1,19 @@
+print ("jelou guorld")
+
+#para hacer comentarios se usa el numeral
+#para ejecutar por consola las instrucciones. click der -> Run Python file in Terminal
+
+age=2
+type(age)
+print("mi edad es ");print(age)
+
+
+name=("jon")
+print("my name is "+name)
+
+
+msg="""Esto es un mensaje
+... con tres saltos 
+... de linea"""
+print(msg)
+
---
+++ Try 1 - 5/if.py
@@ -0,0 +1,10 @@
+numberA=5
+numberB=5
+
+if numberA>numberB:
+    print("el numero A es el mayor") 
+elif numberA<numberB:
+    print("el numero B es el mayor")
+else:
+    print("el numero A y el numero B son iguales")        
+    
