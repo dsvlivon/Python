30e8644c6b66793132137dde788039511ef9fc6f
Daniel <dsvlivon@gmail.com>
2020-05-09 21:59:36 -0300

    clase 19 completada

--- Try 19-25/generadores.py
+++ Try 19-25/generadores.py
@@ -1,6 +1,29 @@
-for i in [1,2]:
-    print("Hola")
+"""
+Se crea una función básica que construya una lista de 
+números pares y devuelva esa lista
+"""
+"""
+def  generaPares(limite):
+    num=1
+    miLista=[]
+   
+    while num<limite:
+        miLista.append(num*2)
+        num=num+1
 
+    return miLista
 
-for j in ["primeavera","verano","otoño","invierno"]:
-    print("La estación es "+j)
+print(generaPares(10))
+"""
+
+def  generaPares(limite):
+    num=1
+    
+    while num<limite:
+        yield num*2
+        num=num+1
+    
+devuelvePares=generaPares(6)
+
+for i in devuelvePares:
+    print(i)
